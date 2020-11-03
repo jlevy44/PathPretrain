d7cc5270becb48251ec2ee967def9f676ca36ec2
jlevy44 <joshualevy44@berkeley.edu>
2020-11-03 13:32:32 -0500

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -226,7 +226,7 @@ def train_model(inputs_dir='inputs_training',
     else:
         # assert not tensor_dataset, "Only ImageFolder and NPYDatasets allowed"
 
-        trainer.model.load_state_dict(torch.load(model_save_loc,map_location=f"cuda:{gpu_id}" if gpu_id>=0 else "cpu"))
+        if os.path.exists(model_save_loc): trainer.model.load_state_dict(torch.load(model_save_loc,map_location=f"cuda:{gpu_id}" if gpu_id>=0 else "cpu"))
 
         if extract_embeddings and extract_embeddings_df:
             assert not semantic_segmentation, "Semantic Segmentation not implemented for whole slide segmentation"
