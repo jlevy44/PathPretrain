803df1a4990c7d9a5d145b0892dd9bba554572f2
jlevy44 <joshualevy44@berkeley.edu>
2022-04-21 16:19:58 -0400

    Update datasets.py

--- pathpretrain/datasets.py
+++ pathpretrain/datasets.py
@@ -121,7 +121,7 @@ class NPYRotatingStack(Dataset):
         i,j=self.ref_index[idx]
         npy=self.cache_npy[i]
         X=self.data[npy]['patches'][j]
-        y=torch.LongTensor([self.data[npy]['patch_info'].iloc[j][self.target_col]])
+        y=torch.LongTensor([self.data[npy]['patch_info'].iloc[j][self.target_col[0]]])
         X=self.transform(self.to_pil(X))
         return X, y
 
