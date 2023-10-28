9e863ab6fb5d06b7a41132d18484814c91b5d110
Joshua Levy <joshualevy44@berkeley.edu>
2023-10-28 16:42:23 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md','r', encoding='utf-8') as f:
       long_description = f.read()
 
 setup(name='pathpretrain',
-      version='0.1.2',
+      version='0.1.3',
       description='Simple setup train image models.',
       url='https://github.com/jlevy44/PathPretrain',
       author='Joshua Levy',
